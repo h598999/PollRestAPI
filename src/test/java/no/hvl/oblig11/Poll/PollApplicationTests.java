ddef3b21ffc7bd24ff0543dca31859d92e4b7f8e
h598999 <h598999@stud.hvl.no>
2024-09-04 09:38:12 +0200

    First commit

---
+++ src/test/java/no/hvl/oblig11/Poll/PollApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.oblig11.Poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
