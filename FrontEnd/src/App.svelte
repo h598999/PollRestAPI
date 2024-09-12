829378072fb83ff7893e42c83e740ae37073262d
h598999 <h598999@stud.hvl.no>
2024-09-12 16:33:46 +0200

    Started on frontend, intied sveltekit!

---
+++ FrontEnd/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let x = 0;
+	let y = 0;
+
+	/** @param {number} value */
+	function yPlusAValue(value) {
+		return value + y;
+	}
+
+	$: total = yPlusAValue(x);
+</script>
+
+Total: {total}
+<button on:click={() => x++}> Increment X </button>
+
+<button on:click={() => y++}> Increment Y </button>
--- src/main/java/no/hvl/oblig11/Poll/controllers/UserController.java
+++ src/main/java/no/hvl/oblig11/Poll/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import no.hvl.oblig11.Poll.models.User;
  * UserController
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1")
 public class UserController {
 
